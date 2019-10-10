9f448930da7c43412aa1ca9cd5b1dffd9ce63855
Ash <5542898+AshIndigo@users.noreply.github.com>
2019-10-10 14:18:07 -0400

    Added a back button to the category list

--- src/main/java/com/ashindigo/guihelpful/gui/CommandCategoryGui.java
+++ src/main/java/com/ashindigo/guihelpful/gui/CommandCategoryGui.java
@@ -4,9 +4,11 @@ import com.ashindigo.guihelpful.api.CommandCategory;
 import com.ashindigo.guihelpful.api.CommandInfo;
 import com.ashindigo.guihelpful.widgets.WLabelButton;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
+import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
 import io.github.cottonmc.cotton.gui.widget.WListPanel;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.LiteralText;
 
 import java.util.function.BiConsumer;
 
@@ -22,6 +24,9 @@ public class CommandCategoryGui extends LightweightGuiDescription {
         };
         WListPanel<CommandInfo, WLabelButton> list = new WListPanel<>(commandCategory.getCommands(), WLabelButton.class, WLabelButton::new, configurator);
         root.add(list, 0, 0, 14, 14);
+        WButton back = new WButton(new LiteralText("Back"));
+        back.setOnClick(() -> MinecraftClient.getInstance().openScreen(new HelpfulScreen(new HelpfulCategoryGui())));
+        root.add(back, 0, 15, 14, 7);
         root.validate(this);
     }
 }
